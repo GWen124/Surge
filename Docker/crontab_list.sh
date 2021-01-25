6c3bbf21c0ec5a394821f3757f44cfe3dfb1cf70
GWen124 <75674512+GWen124@users.noreply.github.com>
2021-01-25 23:09:56 +0800

    Update crontab_list.sh

--- Docker/crontab_list.sh
+++ Docker/crontab_list.sh
@@ -4,6 +4,8 @@
 # 定时更新
 #0 */1 * * * git -C /scripts/ pull |ts >> /scripts/logs/pull.log 2>&1
 
+35 0/1 * * * sh /scripts/docker/docker_entrypoint.sh >> /scripts/logs/docker_entrypoint.log 2>&1
+
 #超级直播间任务赢京豆
 40 21 * * * node /scripts/jd_super.js >> /scripts/logs/jd_super.log 2>&1
 
