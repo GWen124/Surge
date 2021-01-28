05bfc8d44a1f5f8ace63dbae857eb660a740e294
GWen124 <75674512+GWen124@users.noreply.github.com>
2021-01-28 16:42:49 +0800

    Update shell.sh

--- Docker/shell.sh
+++ Docker/shell.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 function JDJS() {
 git clone https://github.com/Tartarus2014/Script.git /Tartarus2014
+npm install
 }
 
  if [ ! -d "/Tartarus2014/" ]; then
-    echo "未检查到shylocks仓库脚本，初始化下载相关脚本"
+    echo "未检查到仓库脚本，初始化下载相关脚本"
     JDJS
 else
     echo "更新脚本文件"
     git -C /Tartarus2014 reset --hard
     git -C /Tartarus2014 pull --rebase
+    npm install --loglevel error
 fi
+
+
 cp -f Tartarus2014/jd*.js /scripts
